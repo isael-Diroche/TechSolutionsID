227046d2c3c858e7e121617b2f8084e6aec5f36c
Isael Diroche <isaeldiroche00@gmail.com>
2023-08-19 21:28:00 -0400

    added: README.md

--- src/tests.py
+++ src/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from apps.blog.models import Post
 # Create your tests here.
 
+
+class MiModeloTestCase(TestCase):
+    
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+    
+    def setUp(self):
+        Post.objects.create(titulo="Ejemplo", contenido="este es un texto de ejemplo", imagen="""C:\isael\Proyectos\DEV-2022\TechSolutionsID\src\static\img\about-us.png""", autor = "Isael", categorias="Ninguna")
